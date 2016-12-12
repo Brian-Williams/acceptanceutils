5521c5d5404e9c067423a2278ef1edbb219b79b8
Brian Williams <brian.williams@actifio.com>
2016-12-12 15:11:31 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Build Status](https://travis-ci.org/Brian-Williams/acceptanceutils.svg?branch=master)](https://travis-ci.org/Brian-Williams/acceptanceutils)
+
+# Acceptance esting tools for use in unit/acceptance testing
+
+This is a set of tools that may be useful for testing.
---
+++ tests/test_surjection.py
@@ -0,0 +1,23 @@
+from testutils import surjection
+
+one = ([1], 1)
+two = ([1, 2], 2)
+three = ([1, 2, 3], 3)
+three_none = (three[0], None)
+three_item_group = (one, two, three, three_none)
+
+
+class TestSurjectiveOptions(object):
+    three_lis = surjection.surjective_options(*three_item_group)
+
+    def test_number_of_lists_returned(self):
+        assert len(type(self).three_lis) == 3
+
+    def test_defaults(self):
+        for _ in range(10):
+            # get a new list every iteration so can't use class attributes
+            lis = surjection.surjective_options(*three_item_group)
+            for li in lis[1:]:
+                assert li[0] == one[1]
+            for li in lis[2:]:
+                assert li[1] == two[1]
